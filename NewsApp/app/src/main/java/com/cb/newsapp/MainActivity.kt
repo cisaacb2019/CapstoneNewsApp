071319289ab41d2f17cb14625a4ccdec32978c93
Collin B <82788579+cisaacb2019@users.noreply.github.com>
2022-09-16 21:37:59 -0600

    -

--- NewsApp/app/src/main/java/com/cb/newsapp/MainActivity.kt
+++ NewsApp/app/src/main/java/com/cb/newsapp/MainActivity.kt
@@ -1,69 +1,149 @@
 package com.cb.newsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.cb.newsapp.RemotesApi.GetArticle
+import com.cb.newsapp.RemotesApi.RetrofitBuilder
 import com.cb.newsapp.databinding.ActivityMainBinding
-import com.cb.newsapp.databinding.ArticleViewBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     //Private var for view binding
     private lateinit var binding: ActivityMainBinding
+    lateinit var NewsRecyclerView: RecyclerView
+    val my_key = "d50ef362876d480eba923021703abbbc"
     //you could move the sourcedummyvalues to a class for cleaner code//
     private val sourcedummyvalues = arrayListOf(
         Source(
             "cnn",
-            "CNN"
-
+            "CNN",
+            Category.SCIENCE,
+            Language.EN,
+            Country.US
         ),
         Source(
             "the_new_york_times",
-            "TheNewYorkTimes"
-
+            "TheNewYorkTimes",
+            Category.ENTERTAINMENT,
+            Language.EN,
+            Country.US
         ),
         Source(
             "",
-            ""
-
+            "",
+            Category.GENERAL,
+            Language.EN,
+            Country.US
         ),
         Source(
             "market_watch",
-            "Market Watch"
-
+            "Market Watch",
+            Category.ENTERTAINMENT,
+            Language.EN,
+            Country.US
         ),
         Source(
             "Morning_Coffee",
-            "Morning Coffee"
-
+            "Morning Coffee",
+            Category.ENTERTAINMENT,
+            Language.EN,
+            Country.US
         ),
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val ArticleApi = RetrofitBuilder.getInstance().create(GetArticle::class.java)
+        GlobalScope.launch {
+            val result = ArticleApi.getArticle(my_key)
+            if (result != null)
+            // Checking the results
+                Log.d("ayush: ", result.body().toString())
+        }
 
         val view = binding.root
         setContentView(view)
 
 
-        val dummyvalues: List<Article> = (NewsService().dummyvalues)
 
 
-        dummyvalues.forEach {
-            ArticleViewBinding.inflate(layoutInflater, binding.articleContainer, true).apply {
 
-                articleName.text = it.title
-                articleAuthor.text = it.author
-                articleDescription.text = it.description
+//        val dummyvalues = NewsService().dummyvalues()
+
+//removed shared prefrences as not needed at this time
+//        val sharedPreference = getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
+//        val getTitle = sharedPreference.getString("titles", null)
+        //check to see if there is a save state and load if there is//
+
+//        if (getTitle != null) {
+//            val intent = Intent(this@MainActivity, DetailView::class.java)
+//            intent.putExtra("title",getTitle)
+//            intent.putExtra("author", sharedPreference.getString("author", ""))
+//            intent.putExtra("description", sharedPreference.getString("description", ""))
+//            intent.putExtra("content", sharedPreference.getString("content", ""))
+//            intent.putExtra("source", sharedPreference.getString("source", ""))
+//            //                intent.putExtra("image",sharedPreference.getString("image","default value"))
+//            startActivity(intent)
+//
+//        } else {
+
+//
+//        dummyvalues.forEach {
+//            ArticleViewBinding.inflate(layoutInflater, binding.articleContainer, true).apply {
+//
+//                articleName.text = it.title
+//                articleAuthor.text = it.author
+//                articleDescription.text = it.description
+//            }
+//        }
+        val adapter = NewsRecyclerAdapter(dummyvalues)
+
+        NewsRecyclerView = findViewById(R.id.NewsRecycleView)
+        NewsRecyclerView.layoutManager = LinearLayoutManager(this)
+        NewsRecyclerView.adapter = adapter
+
+
+
+
+
+
+        adapter.setOnclickItemListener(object : NewsRecyclerAdapter.ArticleViewClickListener {
+            override fun articleClicked(position: Int) {
+//                Toast.makeText(this@MainActivity, "You Clickws item no $position", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this@MainActivity, DetailView::class.java)
+                intent.putExtra("title", dummyvalues[position].title)
+                intent.putExtra("author", dummyvalues[position].author)
+                intent.putExtra("description", dummyvalues[position].description)
+                intent.putExtra("content", dummyvalues[position].content)
+                intent.putExtra("source", dummyvalues[position].publishedAt)
+                intent.putExtra("image", dummyvalues[position].imageUrl)
+                startActivity(intent)
+
+
+                //store shared prefs here (removed as not needed at this point and time//
+//                    var editor = sharedPreference.edit()
+//                    editor.putString("titles", dummyvalues[position].title)
+//                    editor.putString("author", dummyvalues[position].author)
+//                    editor.putString("description", dummyvalues[position].description)
+//                    editor.putString("content", dummyvalues[position].content)
+//                    editor.putString("source", dummyvalues[position].publishedAt)
+//                editor.putString("image",dummyvalues[position].imageUrl)
+//
+//                    editor.commit()
+
             }
-        }
-        
+        })
 
 
     }
-    fun newsServiceFunction(vararg dummyvalues: List<NewsService>) {
 
 
-       
-    }
 }
--- NewsApp/app/src/main/java/com/cb/newsapp/NewsService.kt
+++ NewsApp/app/src/main/java/com/cb/newsapp/NewsService.kt
@@ -1,72 +1,74 @@
 package com.cb.newsapp
 
-import com.cb.newsapp.databinding.ArticleViewBinding
- class NewsService (
+import com.cb.newsapp.Data.Article
+
+class NewsService () {
     //here are some dummy values for an array pass do map of values soon//
-         val dummyvalues: List<Article> = arrayListOf(
-            Article(
-                "", "Breaking News...",
-                "descriptiondisplay", "url",
-                "imageurl", "publishedate",
-                "content"
-            ),
-            Article(
-                "", "Car_Crash",
-                "descriptiondisplay2", "url2",
-                "imageurl2", "publishedate2",
-                "content2"
-            ),
-            Article(
-                "Bill Smith", "Stocks To Buy",
-                "descriptiondisplay3", "url3",
-                "imageurl3", "publishedate3",
-                "content3"
-            ),
-            Article(
-                "dave jones  ", "Politics",
-                "", "url4",
-                "imageurl4", "publishedate4",
-                "content4"
-            ),
-            Article(
-                "some one ", "art stolen",
-                "descriptiondisplay5", "url5",
-                "", "publishedate5",
-                "content5"
-            ),
-
-             Article(
-                 "SCROLL TEST ", "HOPE IT WORKS",
-                 "descriptiondisplay5", "url5",
-                 "", "publishedate5",
-                 "content5"
-             ),
-
-             Article(
-                 "SCROLL TEST ", "HOPE IT WORKS",
-                 "descriptiondisplay5", "url5",
-                 "", "publishedate5",
-                 "content5"
-             ),
-
-             Article(
-                 "SCROLL TEST ", "HOPE IT WORKS",
-                 "descriptiondisplay5", "url5",
-                 "", "publishedate5",
-                 "content5"
-             ),
-             Article(
-                 "SCROLL TEST ", "HOPE IT WORKS",
-                 "descriptiondisplay5", "url5",
-                 "", "publishedate5",
-                 "content5"
-             ),
-             Article(
-                 "SCROLL TEST ", "HOPE IT WORKS",
-                 "descriptiondisplay5", "url5",
-                 "", "publishedate5",
-                 "content5"
-             ),
-
-        )
-)
+//    fun dummyvalues(): List<Article> {
+//         val MainList = listOf(
+////   Article(
+////      arrayOf(),"", "Breaking News...",
+////       "descriptiondisplay", "url",
+////        "imageurl", "publishedate",
+////        "Breaking news something happened...."
+////    ),
+////    Article(
+////        "", "Car_Crash",
+////        "descriptiondisplay2", "url2",
+////        "imageurl2", "publishedate2",
+////        "car crash spotted out near highway."
+////    ),
+////    Article(
+////        "Bill Smith", "Stocks To Buy",
+////        "descriptiondisplay3", "url3",
+////        "imageurl3", "publishedate3",
+////        "This  is a guarentee return on investment I promise....(not financial advice)"
+////    ),
+////    Article(
+////        "dave jones  ", "Politics",
+////        "", "url4",
+////        "imageurl4", "publishedate4",
+////        "Something political happened, some rage others agree."
+////    ),
+////    Article(
+////        "some one ", "art stolen",
+////        "descriptiondisplay5", "url5",
+////        "", "publishedate5",
+////        "1 billion dollar peice has been heisted."
+////    ),
+////
+////    Article(
+////        "SCROLL TEST ", "HOPE IT WORKS",
+////        "descriptiondisplay5", "url5",
+////        "", "publishedate5",
+////        "content5"
+////    ),
+////    Article(
+////        "SCROLL TEST ", "HOPE IT WORKS",
+////        "descriptiondisplay5", "url5",
+////        "", "publishedate5",
+////        "content5"
+////    ),
+////
+////    Article(
+////        "SCROLL TEST ", "HOPE IT WORKS",
+////        "descriptiondisplay5", "url5",
+////        "", "publishedate5",
+////        "content5"
+////    ),
+////    Article(
+////        "SCROLL TEST ", "HOPE IT WORKS",
+////        "descriptiondisplay5", "url5",
+////        "", "publishedate5",
+////        "content5"
+////    ),
+////    Article(
+////        "SCROLL TEST ", "HOPE IT WORKS",
+////        "descriptiondisplay5", "url5",
+////        "", "publishedate5",
+////        "content5"
+////    ),
+//         )
+//         return MainList
+//     }
+    }
